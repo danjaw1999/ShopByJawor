0a47e78c30459afbbe7788e66d7c949731096d55
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-08-03 16:56:45 +0200

    Added React Roouter, and moving after click on produAdded React Roouter, and moving after click on product
    Added React Roouter, and moving after click on produAdded React Roouter, and moving after click on product
    
    Added React Roouter, and moving after click on product
    
    Asddded React Roouter, and moving after click on produc

--- development/js/NavBar.js
+++ development/js/NavBar.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  NavLink,
+  useParams
+} from "react-router-dom";
 import Products from "./Products";
 const API = `http://localhost:3005/`;
-const NavBar = () => {
+
+const Details = () => {
+  let params = useParams();
+
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    fetch(`${API}${params.category}/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <div>{product.title}</div>
+    </>
+  );
+};
+
+const NavBar = (props) => {
   const [menu, setMenu] = useState([]);
   const [sCat, setSCat] = useState([]);
   const [cCat, setCCat] = useState();
@@ -21,25 +47,39 @@ const NavBar = () => {
   };
 
   return (
-    <div className="mainCategories">
-      <h1 className="textMain">Kategorie główne</h1>
-      <ul className="uppCat">
-        {menu?.map((e) => (
-          <li
-            key={uuidv4()}
-            className="liCat"
-            onClick={() => {
-              getCat(e.url);
-              setCCat(e.url);
-            }}
-            value={e.url}
-          >
-            <div className="cat">{e.nameCat}</div>
-          </li>
-        ))}
-      </ul>
-      <Products cat={cCat} />
-    </div>
+    <Router>
+      <div className="mainCategories">
+        <h1 className="textMain">Kategorie główne</h1>
+        <ul className="uppCat">
+          {menu?.map((e) => (
+            <li
+              key={uuidv4()}
+              className="liCat"
+              onClick={() => {
+                getCat(e.url);
+                setCCat(e.url);
+              }}
+              value={e.url}
+            >
+              <div className="cat">{e.nameCat}</div>
+            </li>
+          ))}
+        </ul>
+        <Switch>
+          <Route path="/details/:category/:id">
+            <>
+              <Details />
+            </>
+          </Route>
+          <Route path="/">
+            <Products cat={cCat} />
+          </Route>
+          {/* <Route path="*" component={NavBar}>
+            <p></p>
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 };
 export default NavBar;
--- development/js/Products.js
+++ development/js/Products.js
@@ -1,48 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { NavLink } from "react-router-dom";
 const API = `http://localhost:3005/`;
 const Products = (props) => {
   const [menu, setMenu] = useState([]);
   const [sCat, setSCat] = useState([]);
+  const [category, setCategory] = useState();
 
   useEffect(() => {
-    console.log("products useeffect");
-
+    setCategory(props.cat);
     fetch(`${API}categories`)
       .then((res) => res.json())
       .then((data) => setMenu(data))
       .catch((err) => console.log(err));
-
-    console.log("---------prod.cat", props.cat);
-
     if (props.cat) {
-      console.log("jest kategoria pobieram");
-
       fetch(`${API + props.cat}`)
         .then((res) => res.json())
         .then((data) => setSCat(data));
     }
-  }, []);
-  //   const getCat = (cat) => {
-  //     fetch(`${API + cat}`)
-  //       .then((res) => res.json())
-  //       .then((data) => setSCat(data));
-  //   };
+  }, [props]);
 
   return (
     <>
-      {console.log("return componet products cat:", props)}
       <ul className="mainViewLi">
         {sCat?.map((e) => (
           <li key={uuidv4()}>
-            <div className="mainView">
-              <div>
-                <h2>{e.title}</h2>
-                <NavLink to={`/details/${e.url}/${e.id}`}>hj</NavLink>
-                <p>Zdjęcie</p>
+            <NavLink to={`/details/${e.url}/${e.id}`}>
+              <div className="mainView">
+                <div>
+                  <h2>{e.title}</h2>
+                  <p>Zdjęcie</p>
+                </div>
+                <p className="price">Cena: {e.price}zł</p>
               </div>
-              <p className="price">Cena: {e.price}zł</p>
-            </div>
+            </NavLink>
           </li>
         ))}
       </ul>
